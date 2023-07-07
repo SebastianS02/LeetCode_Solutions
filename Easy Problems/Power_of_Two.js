e3a8c62a16f4c1311a54a0e9274eee15b1d17de1
SebastianS02 <stecsebastian2002@gmail.com>
2023-07-07 10:44:02 -0400

    Power of Two completed and explained (revised)

--- Easy Problems/Power_of_Two.js
+++ Easy Problems/Power_of_Two.js
@@ -1,6 +1,6 @@
 // Given an integer n, return true if it is a power of two. Otherwise, return false.
 
-// An integer n is a power of two, if there exists an integer x such that n == 2x.
+// An integer n is a power of two, if there exists an integer x such that n == 2^x.
 
 /**
  * @param {number} n
