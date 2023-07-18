f7c4dcd4a3eb908dc6ca76aaa9eaf19380ca2201
SebastianS02 <stecsebastian2002@gmail.com>
2023-07-18 10:16:24 -0400

    Reverse String completed and explained

---
+++ Easy Problems/Reverse_String.js
@@ -0,0 +1,14 @@
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    return s.reverse();
+};
+
+//Due to JavaScript's .reverse() function, this question is very easy. The .reverse() function will
+//reverse any array in O(n) time using O(1) extra memory. 
