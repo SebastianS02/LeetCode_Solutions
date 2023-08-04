75b6f93ce125e964ed5500e2b5078d119ca78359
SebastianS02 <stecsebastian2002@gmail.com>
2023-08-04 10:26:09 -0400

    To Lower Case completed and explained

---
+++ Easy Problems/To_Lower_Case.js
@@ -0,0 +1,13 @@
+// Given a string s, return the string after replacing
+// every uppercase letter with the same lowercase letter.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    return s.toLowerCase();
+};
+
+//JavaScript has a built in toLowerCase function which will turn any upper case letter in a string to
+//lower case.
