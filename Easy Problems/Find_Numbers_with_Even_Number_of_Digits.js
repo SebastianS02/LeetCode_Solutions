74a13108d3202b2d20b03b7cf5134c771e5603ba
SebastianS02 <stecsebastian2002@gmail.com>
2023-08-04 11:16:39 -0400

    Find Numbers with Even Number of Digits completed and explained

--- Easy Problems/Find_Numbers_with_Even_Number_of_Digits.js
+++ Easy Problems/Find_Numbers_with_Even_Number_of_Digits.js
@@ -1,4 +1,4 @@
-// Given an array nums of integers, return how many of them contain an even number of digits.
+// Given an array nums of integers, return how many of them contain an even number of digits. 
 
 /**
  * @param {number[]} nums
