20f95512f2e8f9ef45a47f29d36f4c6f05a77a87
SebastianS02 <stecsebastian2002@gmail.com>
2023-08-04 11:15:31 -0400

    Number of Senior Citizens completed and explained

---
+++ Easy Problems/Find_Numbers_with_Even_Number_of_Digits.js
@@ -0,0 +1,20 @@
+// Given an array nums of integers, return how many of them contain an even number of digits.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let count = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i].toString().length % 2 == 0){
+            count++;
+        }
+    }
+    return count;
+};
+
+//We declare a count variable to keep track of how many numbers have an even number of digits. We use
+//a for loop to traverse nums. At each element, we convert it into a string using toString(), and check
+//to see if its length mod 2 returns 0, or if it is evenly divisible by 2. If it is, we increment count
+//by 1. We return count once we exit the for loop.
