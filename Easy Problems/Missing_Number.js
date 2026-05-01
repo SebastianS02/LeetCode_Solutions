0822abbd4448a9d6bb8a4ba12e6923f421bc0c47
SebastianS02 <stecsebastian2002@gmail.com>
2026-05-01 11:21:13 -0400

    Updated Missing_Number solution and explanation

--- Easy Problems/Missing_Number.js
+++ Easy Problems/Missing_Number.js
@@ -6,30 +6,13 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let check = 0;
-    let i = 0;
-    while(i <= nums.length){
-        if(nums[i] == check){
-            check++;
-            i = 0;
-        }
-        else{
-            i++;
-        }
-        if(i == nums.length){
-            return check;
-        }
-    }
-    return nums.length;
+    return ((nums.length * (nums.length + 1)) / 2)
+        - (nums.reduce((sum, num) => sum + num, 0));
 };
 
-//We declare a check variable to track what number we are looking for, always starting at 0. We
-//also declare an i variable to track our place in the array as we iterate through it using the while
-//loop. While i is less than or equal to the length of the array (since the array will always start at 0,
-//if every element is accounted for, we should be able to exit the loop because with every iteration, i
-//increases by 1, meaning if we find every number, i will be greater than the length of the array), we 
-//check to see if the current element is equal to the number we are looking for, always starting at index 0.
-//If we find it, increase check by 1, reset i to 0, and we check for the next number all over again. If we do
-//not find it, go to the next element. If i is equal to the length of the array, meaning we checked every variable
-//and could not find check, we return check, as that is the number we could not find. If we do find every number,
-//we will exit the while loop and return the length of the array.
+//We are doing three things here. First, we find the sum of 0 to n using Gauss's formula, (n * (n + 1)) / 2. Instead of n,
+//we use nums.length, as the array should have all numbers from 0 to n minus the one that is missing, but since arrays
+//are 0 indexed, we don't need to account for that. Once we have the sum, we also need to find the actual sum of nums,
+//which we do using JavaScript's built-in reduce() function, which sums all the elements of nums. Once we have the
+//expected sum (left side of the subtraction) and the actual sum of nums (right side of the subtraction), we subtract the
+//actual sum from the expected sum, which will always give us the missing number.
